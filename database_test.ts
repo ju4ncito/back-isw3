0f4be17b4ad7fa09de2ac91d8c75a3fd6479cd78
ju4ncito <villarrealjm2@gmail.com>
2023-02-18 10:21:14 -0300

    added calculate profit test

--- database_test.ts
+++ database_test.ts
@@ -27,4 +27,30 @@ Deno.test("in memory repository test", async (t) => {
     const transactions = await repository.get('Sapo');
     assertEquals(transactions.length, 2);
   });
+
+  await t.step("calculate profit", async () => {
+    const transaction1: Transaction = {
+      nft: 'Sapo',
+      price: 1.23,
+      status: 'bought',
+    }; 
+    const transaction2: Transaction = {
+      nft: 'Sapo',
+      price: 2.34,
+      status: 'sold',
+    }; 
+    const transaction3: Transaction = {
+      nft: 'Toad',
+      price: 3.45,
+      status: 'sold',
+    }; 
+    const repository = new InMemoryTransactionRepository();
+    await repository.save(transaction1);
+    await repository.save(transaction2);
+    await repository.save(transaction3);
+
+    const profit = await repository.profit('Sapo');
+    assertEquals(profit, transaction2.price - transaction1.price);
+  });
+  
 });
